cbe8e0853cc08fab4ccf9746d729e7113e8148a9
joshuaalbert <albertjoshua5@gmail.com>
2023-09-15 14:24:10 +0200

    refactor the controller

--- src/main/java/de/github/joshu4l/asterixexercise/AsterixController.java
+++ src/main/java/de/github/joshu4l/asterixexercise/AsterixController.java
@@ -1,11 +1,8 @@
 package de.github.joshu4l.asterixexercise;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.List;
 
 
@@ -16,10 +13,15 @@ public class AsterixController {
     // FINAL ATTRIBUTE (declaring an immutable CharacterRepo instance)
     private final CharacterRepo characterRepo;
 
+
     /*
-        ENABLE DEPENDENCY INJECTION HERE
-        By telling the AsterixController-Constructor, that it shall (by default) instantiate a CharacterRepo,
-        we can make use of its methods
+        ENABLE DEPENDENCY INJECTION HERE,
+        by telling the AsterixController-Constructor,
+        that it shall (by default) instantiate a CharacterRepo,
+        whose methods we can use below.
+
+        Special benefit about Dependency Injection:
+        The whole program can use the very same DB instance! :)
      */
     public AsterixController(CharacterRepo characterRepo){
         this.characterRepo = characterRepo;
@@ -34,6 +36,8 @@ public class AsterixController {
          */
         return characterRepo.findAll();
     }
+
+
 
 
 }
